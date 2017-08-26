1468a1fa9895d73476c1368c083c8afe39f14d29
MarKvoff <Data@sjtu.edu.cn>
2017-08-26 17:46:03 +0800

    Add files via upload

---
+++ Sol_53.java
@@ -0,0 +1,30 @@
+package leetcode_sol;
+/**
+ * This is the solution for problems in leetcode.com 
+ * Question 53. Maximum Subarray
+ * 
+ * @author czm
+ *
+ */
+
+public class Sol_53 {
+	
+	public int maxSubArray(int[] nums) {
+		int sum = nums[0];
+		int pos = nums[0];
+		
+		for (int i = 1; i < nums.length; i++) {
+			pos = Math.max(pos + nums[i], nums[i]);
+			sum =  Math.max(pos, sum);
+		}
+		return sum;
+    }
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
