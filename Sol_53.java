0b9a6807ce929f313d80e59faca98e65c68b6757
MarKvoff <Data@sjtu.edu.cn>
2017-08-27 16:05:01 +0800

    Revert "Add files via upload"
    This reverts commit 1a116523b5152e6bab6db89861296f0ea9c7806c.

--- Sol_53.java
+++ Sol_53.java
@@ -9,44 +9,23 @@ package leetcode_sol;
 
 public class Sol_53 {
 	
-	public static int maxSubArray(int[] nums) {
+	
+	//the following is the O(n) solution
+	public int maxSubArray(int[] nums) {
 		int sum = nums[0];
 		int pos = nums[0];
 		
-		
 		for (int i = 1; i < nums.length; i++) {
 			pos = Math.max(pos + nums[i], nums[i]);
-			System.out.println("pos = " + pos);
 			sum =  Math.max(pos, sum);
-			System.out.println("sum = " + sum);
 		}
 		return sum;
     }
 	
 	
-	public static int maxSubArray2(int[] nums) {
-		int sum = nums[0];
-		int end = nums[0];
-		
-		for (int i = 1; i < nums.length; i ++) {
-			//if (sum )
-			
-			
-			
-			
-		}
-		return sum;
-	}
-	
-	
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = {100 ,-30, -4,200};
-		Sol_53.maxSubArray(a);
-		
-		
-		
 
 	}
 
