1a116523b5152e6bab6db89861296f0ea9c7806c
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-08-27 16:02:42 +0800

    Add files via upload

--- Sol_53.java
+++ Sol_53.java
@@ -9,23 +9,44 @@ package leetcode_sol;
 
 public class Sol_53 {
 	
-	
-	//the following is the O(n) solution
-	public int maxSubArray(int[] nums) {
+	public static int maxSubArray(int[] nums) {
 		int sum = nums[0];
 		int pos = nums[0];
 		
+		
 		for (int i = 1; i < nums.length; i++) {
 			pos = Math.max(pos + nums[i], nums[i]);
+			System.out.println("pos = " + pos);
 			sum =  Math.max(pos, sum);
+			System.out.println("sum = " + sum);
 		}
 		return sum;
     }
 	
 	
-
+	public static int maxSubArray2(int[] nums) {
+		int sum = nums[0];
+		int end = nums[0];
+		
+		for (int i = 1; i < nums.length; i ++) {
+			//if (sum )
+			
+			
+			
+			
+		}
+		return sum;
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int[] a = {100 ,-30, -4,200};
+		Sol_53.maxSubArray(a);
+		
+		
+		
 
 	}
 
