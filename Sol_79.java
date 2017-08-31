6fbda754aecc900201fa63618e470f128ddf3d4e
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-08-31 23:55:39 +0800

    Add files via upload

---
+++ Sol_79.java
@@ -0,0 +1,38 @@
+package leetcode_sol;
+/**
+ * This is the solution for problems in leetcode.com 
+ * Question 79. Word Search
+ * 
+ * @author czm
+ *
+ */
+
+
+//under solving
+public class Sol_79 {
+	
+	public boolean exist(char[][] board, String word) {
+		
+		int m = board.length;
+		int n = board[0].length;
+		
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				
+					
+			}
+		}
+		return false;
+        
+    }
+	
+	
+	
+	
+	
+	
+	
+
+	
+
+}
