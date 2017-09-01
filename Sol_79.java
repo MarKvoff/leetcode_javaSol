27bbe045355cc204828a889321979c72a1cf570f
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-09-01 16:04:02 +0800

    Update Sol_79.java

--- Sol_79.java
+++ Sol_79.java
@@ -9,8 +9,8 @@ package leetcode_sol;
 
 
 //under solving
-public class Sol_79 {
-	
+public class Sol_79 {						
+		
 	public boolean exist(char[][] board, String word) {
 		
 		int m = board.length;
@@ -18,21 +18,36 @@ public class Sol_79 {
 		
 		for (int i = 0; i < m; i++) {
 			for (int j = 0; j < n; j++) {
-				
-					
+				if (findWord(i, j, word, board, 0))
+					return true;
 			}
 		}
 		return false;
-        
-    }
+    	}
 	
-	
-	
-	
-	
-	
-	
+	private boolean findWord(int i, int j, String word, char[][] board, int pos){
+		if (word.length() == pos)//find the word
+			return true;
+		if (i == board.length || i < 0 || j < 0 || j == board[0].length)//exceed the boundary
+			return false;
+		if (board[i][j] != word.charAt(pos))//mismatch
+			return false;
+		else {
+			char tmp = board[i][j];
+			board[i][j] = 0;
+			if (findWord(i, j+1, word, board, pos+1))
+				return true;
+			else if (findWord(i, j-1, word, board, pos+1))
+				return true;
+			else if (findWord(i+1, j, word, board, pos+1))
+				return true;
+			else if (findWord(i-1, j, word, board, pos+1))
+				return true;
+			else
+				board[i][j] = tmp;
+				return false;
+		}
+	}
 
-	
 
 }
