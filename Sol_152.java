c7dacace9fd0fdba626c8079de11e3990d1d5e0a
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-09-03 16:42:58 +0800

    Add files via upload

--- Sol_152.java
+++ Sol_152.java
@@ -11,22 +11,25 @@ package leetcode_sol;
 
 public class Sol_152 {
 	public int maxProduct(int[] nums) {
+        
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
         int maxProd = nums[0];
-        int maxEndHere = nums[0];
-        for (int i = 1; i < nums.length; i++){
-            maxEndHere = Math.max(nums[i], maxEndHere*nums[i]);
-            maxProd = Math.max(maxProd, maxEndHere);
-            
+        int maxProdEndHere = nums[0];
+        int minProdEndHere = nums[0];
+        int maxProdhere = 0;
+        int minProdhere = 0;
+        
+        for (int i = 0; i < nums.length; i++){
+            maxProdhere = Math.max(Math.max(maxProdEndHere*nums[i],minProdEndHere*nums[i]),nums[i]);
+            minProdhere = Math.min(Math.min(maxProdEndHere*nums[i],minProdEndHere*nums[i]),nums[i]);
+            maxProdEndHere = maxProdhere;
+            minProdEndHere = minProdhere;
+            if (maxProdEndHere > maxProd)
+                maxProd = maxProdEndHere;
         }
         return maxProd;
-        
     }
-    
-    
-	
-	
-	
-	
-	
-
 }
