aadb5a9a05a2b710abf2b1e949fa362998bf2408
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-09-05 00:26:07 +0800

    Add files via upload

--- Sol_287.java
+++ Sol_287.java
@@ -11,27 +11,45 @@ package leetcode_sol;
 public class Sol_287 {
 	
 	public int findDuplicate(int[] nums) {
-		int target = nums[0];
 		
-        
-    }
-	
-	private boolean scanThrough(int[] nums, int target) {
+		int target = nums[0];
 		int lessThan = 0;
 		int moreThan = 0;
 		int equal = 0;
-		for (int i = 0; i < nums.length; i++) {
-			if (equal > 1)
-				return true;
-			
-			
+		int lo = 0;
+		int hi = nums.length-1;
+		
+		while(true) {
+			for (int i = 0; i < nums.length; i++) {
+				
+				if (target == nums[i])
+					equal++;
+				else if (target > nums[i])
+					lessThan++;
+				else
+					moreThan++;
+				
+				if (equal > 1)
+					return target;
+		
+				if (lessThan > target-1) {
+					hi = target - 1;
+					target = (lo+target)/2;
+					break;
+				}
+				
+				if (moreThan > nums.length-target) {
+					lo = target + 1;
+					target = (target+hi)/2;
+					break;
+					
+				}	
+			}
+			lessThan = 0;
+			moreThan = 0;
+			equal = 0;
+			lo = 0;
+			hi = nums.length-1;
 		}
-		
-		
-		
-	}
-	
-	
-	
-
+    }
 }
