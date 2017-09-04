99239173d52a43898c51fc34b52cecbcc410f0be
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-09-04 23:56:11 +0800

    Add files via upload

---
+++ Sol_287.java
@@ -0,0 +1,37 @@
+package leetcode_sol;
+/**
+ * This is the solution for problems in leetcode.com 
+ * Question 287. Find the Duplicate Number
+ * 
+ * @author czm
+ *
+ */
+
+
+public class Sol_287 {
+	
+	public int findDuplicate(int[] nums) {
+		int target = nums[0];
+		
+        
+    }
+	
+	private boolean scanThrough(int[] nums, int target) {
+		int lessThan = 0;
+		int moreThan = 0;
+		int equal = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (equal > 1)
+				return true;
+			
+			
+		}
+		
+		
+		
+	}
+	
+	
+	
+
+}
