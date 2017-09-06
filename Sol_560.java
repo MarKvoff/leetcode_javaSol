7145054aa4b0ec594985d42898437577edeb9617
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-09-06 23:57:06 +0800

    Add files via upload

---
+++ Sol_560.java
@@ -0,0 +1,27 @@
+package leetcode_sol;
+
+import java.util.*;
+
+/**
+ * This is the solution for problems in leetcode.com 
+ * Question 560. Subarray Sum Equals K
+ * 
+ * @author czm
+ *
+ */
+public class Sol_560 {
+	
+	public int subarraySum(int[] nums, int k) {
+		int count = 0;
+		int sum = 0;
+		
+		HashMap<Integer, Integer> sums = new HashMap<Integer, Integer>();
+		
+		
+		
+		
+		
+        
+    }
+
+}
