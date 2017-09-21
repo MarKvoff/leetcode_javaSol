e7f91797e42ee04f0d05b569a5898023ded83a36
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-09-21 13:25:20 +0800

    Add files via upload

--- Sol_209.java
+++ Sol_209.java
@@ -11,33 +11,24 @@ package leetcode_sol;
 public class Sol_209 {
 	
 	public int minSubArrayLen(int s, int[] nums) {
+		
 		if (nums == null)
 			return 0;
 		
-		int front = 0, end = -1;
+		int front = 0;
 		int sum = 0, minDis = Integer.MAX_VALUE;
+		
 		for (int i = 0; i < nums.length; i++) {
-			if (sum < s) {
-				sum += nums[i];
-				end++;
-			}
-			
-			if (sum >= s) {
-				while (sum >= s) {
-					minDis = Math.min(minDis, end - front +1);
-					sum -= nums[front++];
-				}
-				front--;	
-				sum += nums[i];
-				end++;
+			sum += nums[i];
+		
+			while (sum >= s) {
+				minDis = Math.min(minDis, i - front +1);
+				sum -= nums[front++];
 			}
 			
 		}
 		
-		if (sum < s)
-			return 0;
-		
-		return minDis;
+		return minDis == Integer.MAX_VALUE ? 0 : minDis;
     }
 	
 	public static void main(String[] args) {
@@ -46,7 +37,7 @@ public class Sol_209 {
 		
 		
 		
-		System.out.println(solution.minSubArrayLen(6, nums));
+		System.out.println(solution.minSubArrayLen(1, nums));
 		
 		
 	}
