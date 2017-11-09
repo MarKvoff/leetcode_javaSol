fb7d2019b76be34f95a39016f054daebcb18e9b9
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-11-10 00:41:21 +0800

    Add files via upload

---
+++ Sol_110.java
@@ -0,0 +1,61 @@
+package leetcode_sol;
+
+/**
+ * This is the solution for problems in leetcode.com 
+ * Question 110. Balanced Binary Tree
+ * 
+ * @author czm
+ * 
+ *
+ */
+public class Sol_110 {
+	
+	//top to down method
+	public boolean isBalanced(TreeNode root) {
+		
+		if (root == null)
+			return true;
+		
+		int left = depth(root.left);
+		int right = depth(root.right);
+		
+		if (Math.abs(left - right) <= 1)
+			return isBalanced(root.left) && isBalanced(root.right);
+		else
+			return false;
+        
+    }
+	
+	public int depth(TreeNode node) {
+		if (node == null)
+			return 0;
+		return Math.max(depth(node.left), depth(node.right)) + 1;	
+	}
+	
+	
+	//bottom to up method
+	public boolean isBalancedV2(TreeNode root) {
+	
+		
+		
+	}
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
