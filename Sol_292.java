f074832804ab8daea7ab5170bd7eebfc6cb90d0d
ZHEMIN CHEN <Data@sjtu.edu.cn>
2017-12-01 12:06:49 +0900

    Add files via upload

---
+++ Sol_292.java
@@ -0,0 +1,17 @@
+package leetcode_sol;
+
+/**
+ * This is the solution for problems in leetcode.com 
+ * Question 292. Nim Game
+ * 
+ * @author czm
+ * 
+ *
+ */
+public class Sol_292 {
+	
+	public boolean canWinNim(int n) {
+		return n % 4 != 0;
+    }
+
+}
