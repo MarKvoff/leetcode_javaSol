ec1a5db25fe2c56e99b639bf2f4273b5ace56a62
ZHEMIN CHEN <Data@sjtu.edu.cn>
2018-01-01 22:30:14 +0900

    Add files via upload

---
+++ Sol_617.java
@@ -0,0 +1,39 @@
+package leetcode_sol;
+
+/**
+ * This is the solution for problems in leetcode.com
+ * Question 617. Merge Two Binary Trees
+ * 
+ * @author czm
+ * 
+ *
+ */
+public class Sol_617 {
+
+	
+	public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+		
+		if (t1 == null  && t2 == null)
+			return null;
+		else if (t1 == null)
+			return t2;
+		else if (t2 == null)
+			return t1;
+		
+		TreeNode node = new TreeNode(t1.val + t2.val);
+				
+		node.left = mergeTrees(t1.left, t2.left);
+		node.right = mergeTrees(t1.right, t2.right);
+		
+		return node;
+    }
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
