f827a7583762df235a3f24038f39e72fc84843de
ZHEMIN CHEN <Data@sjtu.edu.cn>
2018-01-10 00:42:04 +0900

    Add files via upload

--- Sol_3.java
+++ Sol_3.java
@@ -2,41 +2,45 @@ package leetcode_sol;
 import java.util.*;
 
 /**
- * This is the solution for problems in leetcode.com
- * Question 3. Longest Substring Without Repeating Characters
+ * This is the solution for problems in leetcode.com 
+ * Question 3. Longest Substring Without Repeating Characters 
  * 
  * @author czm
- * 
  *
  */
 public class Sol_3 {
 	
-	public int lengthOfLongestSubstring(String s) {
+	public static int lengthOfLongestSubstring(String s) {
 		
 		if (s.length() == 0)
-            return 0;
+			return 0;
 		
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
-		int pos = 0, lo = 0;
-		int maxLen =0;
+		int lo = 0, pos = 0;
+		int maxLen = 0;
 		
 		while (pos < s.length()) {
-			if (map.containsKey(s.charAt(pos))) {
+			
+			if (map.containsKey(s.charAt(pos)))
+				lo = Math.max(map.get(s.charAt(pos)) + 1, lo);
 				
-				
-			}
 			map.put(s.charAt(pos), pos);
-			maxLen = Math.max(maxLen, pos - lo +1);
+			maxLen = Math.max(maxLen, pos - lo + 1);
+			System.out.println("lo is: " + lo + ", pos is: " + pos );
 			pos++;
+			
+			
 		}
 		
-		return maxLen;   
+		return maxLen;
     }
-	
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		String a = "abba";
+		
+		System.out.println(Sol_3.lengthOfLongestSubstring(a));
 
 	}
 
