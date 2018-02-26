6bba06837e94c7dd6b4950e2391402403e95bf97
ZHEMIN CHEN <Data@sjtu.edu.cn>
2018-02-26 17:00:42 +0800

    Add files via upload

---
+++ Sol_395.java
@@ -0,0 +1,26 @@
+package leetcode_sol;
+
+/**
+ * This is the solution for problems in leetcode.com
+ * Question 395. Longest Substring with At Least K Repeating Characters
+ * 
+ * @author czm
+ * 
+ */
+public class Sol_395 {
+	
+	public int longestSubstring(String s, int k) {
+		
+		
+		
+		
+		
+        
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
