67a3e622efc9b0140a3e3e8b55ab940baeef480b
ZHEMIN CHEN <Data@sjtu.edu.cn>
2018-02-28 20:38:03 +0800

    Add files via upload

---
+++ Sol_450.java
@@ -0,0 +1,47 @@
+package leetcode_sol;
+
+/**
+ * This is the solution for problems in leetcode.com
+ * Question 450. Delete Node in a BST
+ * 
+ * @author czm
+ * 
+ */
+public class Sol_450 {
+	
+	public TreeNode deleteNode(TreeNode root, int key) {
+	    if(root == null){
+	        return null;
+	    }
+	    if(key < root.val){
+	        root.left = deleteNode(root.left, key);
+	    }else if(key > root.val){
+	        root.right = deleteNode(root.right, key);
+	    }else{
+	        if(root.left == null){
+	            return root.right;
+	        }else if(root.right == null){
+	            return root.left;
+	        }
+	        
+	        TreeNode minNode = findMin(root.right);
+	        root.val = minNode.val;
+	        root.right = deleteNode(root.right, root.val);
+	    }
+	    return root;
+	}
+
+	private TreeNode findMin(TreeNode node){
+	    while(node.left != null){
+	        node = node.left;
+	    }
+	    return node;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
